a5b356eb39e0dbda880bdebf428873775ed75168
pkellz <pkellz@github.com>
2017-11-19 01:56:10 -0500

    IT WORKS

--- AngularJS Blog/js/slider.js
+++ AngularJS Blog/js/slider.js
@@ -1,20 +1,20 @@
 // Jquery with no conflict
 jQuery(document).ready(function($) {
-	
+
 	//##########################################
 	// LOF SLIDER
 	//##########################################
-	 
-	 
-	var buttons = { previous:$('#home-slider .button-previous') ,
-						next:$('#home-slider .button-next') };	
-	
 
-	
+
+	var buttons = { previous:$('#home-slider .button-previous') ,
+						next:$('#home-slider .button-next') };
+
+
+
 	function initLofSlider(items, nh, nw, w){
 		$('#home-slider').lofJSidernews( {
 			interval 		: 4000,
-			direction		: 'opacitys',	
+			direction		: 'opacitys',
 			easing			: 'easeInOutExpo',
 			duration		: 1200,
 			auto		 	: false,
@@ -25,30 +25,30 @@ jQuery(document).ready(function($) {
 			mainWidth		:  w,
 			buttons: buttons
 		});
-	}	
-	
-	
+	}
+
+
 	// responsive slider function
-	
+
 	// Default values for page
 	var items = 5;			// Max item display
 	var nh = 73;			// Navigator Height
 	var nw = 188;			// Navigator Width
 	var w = 940;			// Main Width
-		
-	initLofSlider(items, nh, nw, w);	
-	
+
+	initLofSlider(items, nh, nw, w);
+
 	$(window).resize(function(){
-		
+
 		var ww = $(window).width();
 		console.log(ww);
-		
-		// Default Layout: 992px.	
+
+		// Default Layout: 992px.
 		items = 5;
 		nh = 73;
 		nw = 188;
 		w = 940;
-		
+
 		/*
 		// Tablet Layout: 768px.
 		if( ww >= 768 && ww <= 991){
@@ -56,10 +56,10 @@ jQuery(document).ready(function($) {
 			nh = 73;
 			nw = 188;
 			w = 768;
-			
-			console.log('TABLET');	
+
+			console.log('TABLET');
 		}
-		
+
 
 		// Wide Mobile Layout: 480px
 		if( ww >= 480 && ww <= 767){
@@ -67,32 +67,13 @@ jQuery(document).ready(function($) {
 			nh = 73;
 			nw = 188;
 			w = 480;
-				
+
 		}
-		
-		initLofSlider(items, nh, nw, w); */	
-	
+
+		initLofSlider(items, nh, nw, w); */
+
 	});
-	
 
-//close			
+
+//close
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
